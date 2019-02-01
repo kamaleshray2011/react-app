daad3561bf3e9f0f6bdc7675ab37278a4f40f26f
kamaleshray2011 <46379436+kamaleshray2011@users.noreply.github.com>
2019-02-02 00:13:01 +0600

    my first react component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import First from './First/First'
+
+class App extends Component {
+    render() {
+        return ( <div className = "App" >
+            <First / >
+            <First / >
+            <First / >
+            </div >
+        );
+    }
+}
+
+export default App;
---
+++ src/components/First/First.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class First extends Component {
+
+    render(){
+        return(
+        <div>
+        <h3>I am Kamalesh Roy</h3>
+        <h4>I am a software engineer</h4>
+        <p>I also a digital marketer</p>
+        </div>
+        )
+    } 
+    
+}
+export default First
